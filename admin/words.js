ee9ec0a09aaf65d457d6f49f29e80c34af57889f
Frank Petersohn <frank.petersohn@schulportal.hessen.de>
2022-06-09 21:10:00 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'oekofen-pellematic adapter settings': {
+		'en': 'Adapter settings for oekofen-pellematic',
+		'de': 'Adaptereinstellungen für oekofen-pellematic',
+		'ru': 'Настройки адаптера для oekofen-pellematic',
+		'pt': 'Configurações do adaptador para oekofen-pellematic',
+		'nl': 'Adapterinstellingen voor oekofen-pellematic',
+		'fr': "Paramètres d'adaptateur pour oekofen-pellematic",
+		'it': "Impostazioni dell'adattatore per oekofen-pellematic",
+		'es': 'Ajustes del adaptador para oekofen-pellematic',
+		'pl': 'Ustawienia adaptera dla oekofen-pellematic',
+		'zh-cn': 'oekofen-pellematic的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
